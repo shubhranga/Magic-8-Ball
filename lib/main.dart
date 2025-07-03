d3858cd79f08c22421ab586c34e845986d05cae2
Shubham Ranga <shubhamranga30@gmail.com>
2025-07-03 11:05:33 +0530

    Update pubspec.yaml to include new image assets and modify README.md to add project title. Enhance main.dart with comments outlining future development tasks for the Magic-8-Ball application.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false, 
         home: null
       ),
     );
+
+// 2. TODO: Create a stateless widget for the main page
+
+
+
+
+//3. Stateful widget for the ball page
+
+
+
+
+//4. TODO: Add image and make it interactive (add a button)
+
+//5. TODO: Add a random number generator
+
+
+//6. TODO: Add a function to generate a random number
+
+
+
+
+
+
+
 
